1b375c0cfe6d2dd900c69d6b8d8d07e3000a23bc
Anatoly <antauren@yandex.ru>
2021-01-28 02:10:26 +0300

    delete unnecessary import

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,5 @@
 import datetime as dt
 
-from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
